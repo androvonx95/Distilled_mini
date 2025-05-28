47dbf06d217800836a1bf1074748b631fa61d262
androvonx95 <androvonx95@tutamail.com>
2025-05-28 15:02:43 +0530

    commit

--- src/train.py
+++ src/train.py
@@ -38,7 +38,7 @@ def main():
         save_steps=100,
         logging_steps=10,
         no_cuda=not torch.cuda.is_available(),   #no_cuda=True,
-        evaluation_strategy="no",
+        # evaluation_strategy="no",
         report_to="none"
     )
 
